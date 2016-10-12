d6995e0b13fd4b50f13ad509a645f4cf8786f4dc
jarek <jarek.gancarz@wedzony.com.pl>
2016-10-12 15:00:08 +0200

    SQL connection working

--- TKMManager/MainWindow.xaml.cs
+++ TKMManager/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace TKMManager
 {
@@ -19,9 +20,39 @@ namespace TKMManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SqlConnection myConnection;
         public MainWindow()
         {
+           myConnection = new SqlConnection("user id=tkmmanagerdb;" +
+                                       "password=Oz781It_2!30;server=mssql3.gear.host;" +
+                                       "Trusted_Connection=no;" +
+                                       "database=tkmmanagerdb; " +
+                                       "connection timeout=30");
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+            }
+
+            SqlCommand myCommand = new SqlCommand("INSERT INTO dbo.Products (ProductID, ProductName) Values (3, 'Prod3')", myConnection);
+            myCommand.ExecuteNonQuery();
+
+            try
+            {
+                myConnection.Close();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+            }
         }
     }
 }
